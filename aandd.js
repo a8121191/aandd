a41f7c32e7a1338cea969bc6a64cb70ad2fe6f4e
Naoto Hagino <a8121191@gsuite.si.aoyama.ac.jp>
2023-04-30 07:35:32 +0900

    prac1-8

--- aandd.js
+++ aandd.js
@@ -1,5 +1,12 @@
-function createSequentialNumbers(array, n){
+/*function createSequentialNumbers(array, n){
     for(var j=0; j<n; j++){
         array.push(j+1);
+    }
+}*/
+
+function createRandomNumbers(array,n){
+    for(var k=0; k<n; k++){
+        var x = Math.floor(Math.random()*10+1);
+        array.push(x);
     }
 }
