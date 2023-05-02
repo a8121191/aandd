4af2298593e923ee33e8575f3ae3af340b9597c6
Naoto Hagino <a8121191@gsuite.si.aoyama.ac.jp>
2023-05-02 22:43:11 +0900

    aandd

--- aandd.js
+++ aandd.js
@@ -1,3 +1,16 @@
+function show(array) {
+    var html = '';
+    html = html + '<table border="1">';
+    html = html + '<tr>';
+    for (var i = 0; i < array.length; i++) {
+        html = html + '<td>' + array[i] + '</td>';
+    }
+    html = html + '</tr>';
+    html = html + '</table>';
+    //document.getElementById('res').innerHTML = html;
+    document.getElementById('res').insertAdjacentHTML('beforeend', html);
+}
+
 /*function createSequentialNumbers(array, n){
     for(var j=0; j<n; j++){
         array.push(j+1);
