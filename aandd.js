5487da9f8378d3b130a61071c862e69d6ef224b6
Naoto Hagino <a8121191@gsuite.si.aoyama.ac.jp>
2023-05-03 12:21:26 +0900

    aandd

--- aandd.js
+++ aandd.js
@@ -1,4 +1,5 @@
-function show(obj) {
+//ex3-1からコピー
+/*function show(obj) {
     if (Array.isArray(obj)) {
         var html = '';
         html = html + '<table border="1">';
@@ -13,6 +14,28 @@ function show(obj) {
     } else {
         var string = JSON.stringify(obj);
         string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
+    }
+}*/
+
+//ex3-1からコピーしたものを書き換え
+function show(obj,name) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', name);
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', name);
         document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
 }
